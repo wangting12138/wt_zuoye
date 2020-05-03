22926b742426a0f06800c0d74a65b2ab25851e4a
wangting12138 <2639101265@qq.com>
2020-05-03 16:01:50 +0800

    2017010386 王挺

---
+++ src/main/java/com/wt/zuoye/ZuoyeApplication.java
@@ -0,0 +1,13 @@
+package com.wt.zuoye;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuoyeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuoyeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wt/zuoye/ZuoyeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wt.zuoye;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuoyeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
